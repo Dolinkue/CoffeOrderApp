60cc6580c31e4101359cf6119a6ac0fb8879a44b
dolinkue <dolinkue_n@me.com>
2022-06-08 15:55:23 -0300

    Initial Commit

---
+++ CoffeApp/CoffeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeAppApp.swift
+//  CoffeApp
+//
+//  Created by Nicolas Dolinkue on 08/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoffeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
